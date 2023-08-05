d22cf2959b25ce5bdc50eb3186c7574d1df20543
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-08-05 04:45:29 -0300

    Updating notes table

--- APP/models/notes_model.py
+++ APP/models/notes_model.py
@@ -1,28 +1,28 @@
 from APP import db
-from sqlalchemy.sql import func
+from datetime import datetime
 
 
 class Note(db.Model):
 
     def get_formatted_time():
 
-        current_time = func.now()
-        formatted_time = current_time.strftime("%H:%M:%S")
+        current_time = datetime.now()
+        formatted_time = current_time.replace(microsecond=0)
         return formatted_time
 
 
     __tablename__="notes"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     note_name = db.Column(db.String(50), unique=True, nullable=False)
-    data = db.Column(db.String(10000), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default = get_formatted_time, nullable=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     usuario = db.relationship("User", back_populates="notes")
 
-    def __init__(self, note_name,  data, user_id):
+    contents = db.relationship("Note_Content", back_populates="note", lazy="dynamic")
+
+    def __init__(self, note_name, user_id):
         self.note_name = note_name
-        self.data = data
         self.user_id = user_id
 
 
