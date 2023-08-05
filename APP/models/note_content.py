bb2b7504f22c81c3d79063da2fd9b1c15eb4a157
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-08-05 04:32:39 -0300

    Adding Note Content database

---
+++ APP/models/note_content.py
@@ -0,0 +1,16 @@
+from APP import db
+from datetime import datetime
+
+class Note_Content(db.Model):
+    __tablename__ = "note_content"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True, nullable = False)
+    content = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.Date, nullable=False,default = datetime.now().date())
+
+    note_id = db.Column(db.Integer, db.ForeignKey('notes.id'))
+    note = db.relationship("Note", back_populates="contents")
+
+    def __init__(self,content, note_id):
+        self.content = content
+        self.note_id = note_id
+    
