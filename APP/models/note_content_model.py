bd855955c1e65629b4e0ac5b0e93e0fb4b2652c1
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-08-06 04:48:59 -0300

    Updating Note_Content table

---
+++ APP/models/note_content_model.py
@@ -0,0 +1,19 @@
+from APP import db
+from datetime import datetime
+
+class Note_Content(db.Model):
+    __tablename__ = "note_content"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True, nullable = False)
+    content = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.Date, nullable=False,default = datetime.now().date())
+    is_completed = db.Column(db.Boolean, nullable = False, default = False)
+
+    note_id = db.Column(db.Integer, db.ForeignKey('notes.id'))
+    note = db.relationship("Note", back_populates="contents")
+
+    def __init__(self,content, note_id):
+        self.content = content
+        self.note_id = note_id
+
+    
+    
