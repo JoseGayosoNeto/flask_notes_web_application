1df8910ef4a23cbf6fbab1adb8b47f97148478a3
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-08-07 00:01:56 -0300

    Adding update and delete routes for content

--- APP/views/note_content_views.py
+++ APP/views/note_content_views.py
@@ -27,6 +27,7 @@ def add_note_content(id):
             new_note_content = note_content_model.Note_Content(content=content, note_id=id)
             db.session.add(new_note_content)
             db.session.commit()
+            flash(f"{content} has been added to your note list.", "success")
             return redirect(url_for("content.list_content", id=id))
 
     return redirect(url_for("content.list_content", id=id))
@@ -43,3 +44,25 @@ def is_completed_checkbox(content_id):
         current_note_content = note_content_model.Note_Content.query.filter_by(id=content_id).first()
         return redirect(url_for('content.list_content', id=current_note_content.note_id))
     return redirect(url_for("content.list_content", id=current_note_content.note_id))
+
+@content.route('/note/content/id/<int:content_id>/update_content', methods=["GET", "POST"])
+@login_required
+def update_note_content(content_id):
+    content_data = note_content_model.Note_Content.query.filter_by(id=content_id).first()
+    old_content_data = content_data.content
+    note_content_id = content_data.note_id
+    if request.method == "POST":
+        new_content_data = request.form.get("new_notecontent")
+        content_data = note_content_model.Note_Content.query.filter_by(id=content_id).update({"content": new_content_data})
+        db.session.commit()
+        flash(f"{old_content_data} has been updated to {new_content_data}.", "success_update")
+        return redirect(url_for("content.list_content", id = note_content_id))
+    return render_template("update_note_content.html", content=content_data)
+
+@content.route('/note/content/id/<int:content_id>/delete_content')
+@login_required
+def delete_note_content(content_id):
+    content_data = note_content_model.Note_Content.query.filter_by(id=content_id).first()
+    db.session.delete(content_data)
+    db.session.commit()
+    return redirect(url_for("content.list_content", id = content_data.note_id))
