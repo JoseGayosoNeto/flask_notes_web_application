c3a043c1b727f9ffc97dfde76e039eaeea617143
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-10-06 11:26:00 -0300

    Updating APP __init__

--- APP/__init__.py
+++ APP/__init__.py
@@ -20,8 +20,8 @@ app.config["MAIL_SERVER"] = 'smtp.office365.com'
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
-app.config["MAIL_DEFAULT_SENDER"] = 'josegayosoneto@outlook.com.br'
-app.config["MAIL_USERNAME"] = 'josegayosoneto@outlook.com.br'
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get('MAIL_DEFAULT_SENDER')
+app.config["MAIL_USERNAME"] = os.environ.get('MAIL_USERNAME')
 app.config["MAIL_PASSWORD"] = os.environ.get('MAIL_PASSWORD')
 
 
